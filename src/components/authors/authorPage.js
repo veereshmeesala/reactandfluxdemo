459f97a5ab8e15f33b21f4605e2b88993d4a4c8b
veereshmeesala <veereshmeesala@gmail.com>
2016-08-23 21:18:01 +0100

    added basic app structure &  home, about, authors component

---
+++ src/components/authors/authorPage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+
+var Authors = React.createClass({
+    render: function(){
+            return (
+                <div>
+                    <h1>Authors</h1>
+                </div>
+            );
+    }
+});
+
+module.exports = Authors;
