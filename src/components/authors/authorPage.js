130ca6aaf2adc68788243a8ea62c5e8ee6ec2f6c
veereshmeesala <veereshmeesala@gmail.com>
2016-08-23 23:12:07 +0100

    added routing

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,16 +1,30 @@
-'use strict';
+"use strict";
 
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
 
-var Authors = React.createClass({
-    render: function(){
-            return (
-                <div>
-                    <h1>Authors</h1>
-                </div>
-            );
-    }
+var AuthorPage = React.createClass({
+	getInitialState: function() {
+		return {
+			authors: []
+		};
+	},
+
+	componentDidMount: function() {
+		if (this.isMounted()) {
+			this.setState({ authors: AuthorApi.getAllAuthors() });
+		}
+	},
+
+	render: function() {
+		return (
+			<div>
+				<h1>Authors</h1>
+				<AuthorList authors={this.state.authors} />
+			</div>
+		);
+	}
 });
 
-module.exports = Authors;
+module.exports = AuthorPage;
